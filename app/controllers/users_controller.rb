19f9b09afc1562cd5d65a76a0423ec659cbac3a1
Masato Tokuda <mtokuda0609@gmail.com>
2017-06-13 14:41:17 +0000

    timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
   def index
-    @users = User.all.page(params[:pages]).per(10)
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
@@ -62,7 +62,6 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
 
   private
   
